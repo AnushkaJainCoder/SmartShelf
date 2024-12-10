24b34711caa2f2691e884c0083b82dfcc0e1634d
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-10 12:47:50 +0530

    successfully get all the book info with image based on the search input in list form

--- src/Components/SearchBook.jsx
+++ src/Components/SearchBook.jsx
@@ -1,12 +1,32 @@
+import axios from "axios";
 import { useState } from "react"
 
-export default function SearchBook(){
+export default function SearchBook({book}){
     const [text, setText] = useState('');
+    const [books, setBooks] = useState([]);
+
+    const searchBookFilter = async() =>{
+        const res = await axios.get(`https://www.googleapis.com/books/v1/volumes?q=${text}`)
+        setBooks(res.data.items);
+    }
     // const [books, setBooks] = 
     return (
         <>
              <input type="text" value = {text} onChange={(e) => setText(e.target.value)}  placeholder="Search..."/>
-             <button onClick={SearchBook}
+             <button onClick={searchBookFilter}>Search</button>
+             <ul>
+                {books.map((b) =>(
+                   <li key={b.id}>
+                    {b.volumeInfo.title}
+                    {b.volumeInfo.authors}
+                    {/* {b.imageLinks.thumbnail && } */}
+                    <img src={b.volumeInfo.imageLinks.thumbnail} />
+                    
+                    
+
+                   </li>
+                ))}
+             </ul>
    
         </>
     )
