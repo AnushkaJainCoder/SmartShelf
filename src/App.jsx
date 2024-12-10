6e3a37585a4cec0a3f9c0fb25d536b89fa541e06
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-10 15:33:40 +0530

    homepage component started

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,17 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import './App.css'
-// import SearchBook from './Components/SearchBook'
 import BookSearchPage from './Pages/BookSearchPage'
 
-
 function App() {
-  // const [count, gitsetCount] = useState(0)
-
   return (
     <>
-     {/* <SearchBook /> */}
      <Router>
         <Routes>
-          <Route path='/' element = { <BookSearchPage />}>
+          <Route path='/search' element = { <BookSearchPage />}>
           </Route>
         </Routes>
      </Router>
-     {/* <BookSearchPage /> */}
-     {/* helloh */}
     </>
   )
 }
