759945fd9989eb0593aa126a777de5975ad7ebfa
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-11 13:09:28 +0530

    searching done based n input

--- src/Components/SearchBook.jsx
+++ src/Components/SearchBook.jsx
@@ -5,69 +5,69 @@ import Book from "./Book";
 import '../index.css'
 // import { useNavigate } from "react-router-dom";
 
-export default function SearchBook(){
+export default function SearchBook() {
     const [text, setText] = useState('');
     const [filterbooks, setFilterBooks] = useState([]);
     const [allBooks, setAllBooks] = useState([]);
     // const navigate = useNavigate();
 
-    useEffect(()=>{
+    useEffect(() => {
         fetch('/data.json')
-        .then(res => {
-            if(!res.ok){
-                throw new Error('Network response not ok');
+            .then(res => {
+                if (!res.ok) {
+                    throw new Error('Network response not ok');
+                }
+                return res.json();
+            })
+            .then(data => {
+                console.log(data.items);
+                setAllBooks(data.items);
+
+            })
+    }, [])
+
+    const searchBookFilter = () => {
+        const fb = allBooks.filter(book => {
+            const volumeInfo = book.volumeInfo || {};
+            const title = volumeInfo.title || '';
+            if (typeof title === 'string') {
+                return title.toLowerCase().includes(text.toLowerCase());
             }
-            return res.json();
-        })
-        .then(data => {
-            console.log(data.items);
-            setAllBooks(data.items);
-            
-        })
-    },[])
+            return false;
 
-    const searchBookFilter = () =>{
-       const fb = allBooks.filter(book => {
-        const volumeInfo  = book.volumeInfo || {};
-        const title = volumeInfo || '';
-        if(typeof title === 'String'){
-            return title.toLowerCase().includes(text.toLowerCase());
-        }
-        return [];
-
-       });
-       setFilterBooks(fb)
+        });
+        setFilterBooks(fb);
+        // const(set)
+        console.log(filterbooks);
+        
     }
-    // const [books, setBooks] = 
+
     return (
         <>
-         {/* <button onClick={() => navigate('/')}>Back to shelf</button> */}
-           
-             <input type="text" value = {text} onChange={(e) => setText(e.target.value)}  placeholder="Search..."/>
-             <button onClick={searchBookFilter}>Search</button>
-             <div style={{}}>
-             {filterbooks.map((b) =>{
-                const volumeInfo = b.volumeInfo || {};
-                const title = volumeInfo.title || 'No title';
-                const authors = volumeInfo.authors || 'Unknown';
-                const imageLinks = volumeInfo.imageLinks || 'No link';
-                const image = imageLinks.thumbnail || 'No image';
-                
-                return(
-                    <div key={b.id}>
-                        {/* {title}
-                        {authors} */}
-                         <Book 
-                        b = {b} 
-                        title = {title }
-                        author = {authors}
-                        image = {image}/>
-                    </div>
-                )
-             })}
-             
-             </div>
-   
+            <input type="text" value={text} onChange={(e) => setText(e.target.value)} placeholder="Search..." />
+            <button onClick={searchBookFilter}>Search</button>
+            <div style={{}}>
+                {filterbooks.map((b) => {
+                    const volumeInfo = b.volumeInfo || {};
+                    const title = volumeInfo.title || 'No title';
+                    const authors = volumeInfo.authors || 'Unknown';
+                    const imageLinks = volumeInfo.imageLinks || 'No link';
+                    const image = imageLinks.thumbnail || 'No image';
+
+                    return (
+                        <div key={b.id}>
+                            
+                            <Book
+                                b={b}
+                                title={title}
+                                author={authors}
+                                image={image} />
+                        </div>
+                    )
+                })}
+
+            </div>
+
         </>
     )
 }
