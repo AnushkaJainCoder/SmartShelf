f2eada7ad8c21d409f7331b06b8115e78f2af2c4
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-11 15:49:33 +0530

    dropdown styling

--- src/Components/DropDown.jsx
+++ src/Components/DropDown.jsx
@@ -1,9 +1,24 @@
 import { useState } from "react";
 import '../index.css';
 
-export default function DropDown(){
+export default function DropDown({book}){
 
     const [selectedOption, setSelectedOption] = useState();
+    const wtr = [];
+    const read = [];
+    const curr = [];
+
+
+    if(selectedOption === 'Want to read'){
+        wtr.push(book);
+    }
+    else if(selectedOption === 'Read'){
+        read.push(book);
+    }
+    else if(selectedOption === 'Currently Reading'){
+        curr.push(book);
+    }
+    
     const handleChange = (e) =>{
         setSelectedOption(e.target.value )
     }
@@ -11,8 +26,13 @@ export default function DropDown(){
     return (
         <>
         <div className="dropdown">
-            <select value={selectedOption} onChange={handleChange} className="dropdown-select">
+            <select onChange={handleChange} className="dropdown-select">
                 <option ><span className="icon"  >▼</span></option>
+                
+                <option value='Currently ' disabled>Move to....</option>
+                
+                <option >Currently Reading</option>
+                
                 <option >Want to read</option>
                 <option >Read</option>
                 <option >None</option>
