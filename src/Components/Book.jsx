3cc04cc0affd238de7110f248b10de1bdedb01d9
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-13 23:01:00 +0530

    card styling

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -9,7 +9,7 @@ export default function Book({ b, title, author, image }) {
             <img src={image} />
             <DropDown book={b} />
             <div style={{fontSize: '20px'}}>{title}</div>
-            <div className="auth" style={{ color: 'rgb(225 53 53)', fontSize: '12px'}}><i>~{author}</i></div>
+            <div className="auth" style={{ color: 'rgb(162 37 37)', fontSize: '12px',  fontWeight: '500'}}><i>~{author}</i></div>
         </div>
     )
 }
