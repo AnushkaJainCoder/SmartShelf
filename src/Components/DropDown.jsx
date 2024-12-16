3ffb6bde355d90f9533785b7c91336953c5414fd
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-16 17:51:50 +0530

    format file

--- src/Components/DropDown.jsx
+++ src/Components/DropDown.jsx
@@ -29,8 +29,8 @@ export default function DropDown({ book }) {
 
                 <select onChange={handleChange} className="dropdown-select" value={selectedOption}>
 
-                    <option ><span className="icon"  >▼</span></option>
-                    <option value='' disabled>Move to....</option>
+                    <option ><span className="icon"  disabled>▼</span></option>
+                    {/* <option value='' disabled>Move to....</option> */}
                     <option value='Currently Reading'>Currently Reading</option>
                     <option value='Want to read'>Want to read</option>
                     <option value='Read'>Read</option>
