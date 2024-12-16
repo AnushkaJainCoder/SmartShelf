942a09019ca3f4113453dd84429958c29842e33e
anushka.jain <anushka.jain@innovasolutions.com>
2024-12-16 17:43:12 +0530

    add alternative text to the image

--- src/Components/Book.jsx
+++ src/Components/Book.jsx
@@ -8,7 +8,7 @@ const Book = React.memo(({ b, title, author, image })  =>{
 
     return (
         <div className="card">
-            <img src={image} />
+            <img src={image} alt={`${title} cover`}/>
             <DropDown book={b} />
             <div style={{fontSize: '20px'}}>{title}</div>
             <div className="auth" style={{ color: 'rgb(255 222 222)', fontSize: '12px', fontWeight: '700'}}><i>~{author}</i></div>
